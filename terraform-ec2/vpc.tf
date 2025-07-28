473482d93efa12a5cca063ff6e709bda2344d372
Akhilaitgithub <akhilar3108@gmai.com>
2025-07-28 12:48:42 +0530

    Initial commit

---
+++ terraform-ec2/vpc.tf
@@ -0,0 +1,15 @@
+# main.tf
+
+provider "aws" {
+  region = "us-east-1"  # change to your preferred region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
